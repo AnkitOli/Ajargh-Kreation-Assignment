a606525d7e24d68134fccee99d9a530e5767d5a9
Ankit Oli <77269328+AnkitOli@users.noreply.github.com>
2024-03-25 15:52:04 +0530

    Update BusinessEntityService.java

--- src/main/java/com/AjarghKreation/Controller/BusinessEntityService.java
+++ src/main/java/com/AjarghKreation/Controller/BusinessEntityService.java
@@ -1,10 +1,1 @@
-package com.AjarghKreation.Controller;
-import com.AjarghKreation.model.BusinessEntity;
-import java.util.List;
-
-public interface BusinessEntityService {
-    BusinessEntity createBusinessEntity(BusinessEntity businessEntity);
-    List<BusinessEntity> getAllBusinessEntities();
-    BusinessEntity updateBusinessEntity(Long id, BusinessEntity updatedEntity);
-}
 
